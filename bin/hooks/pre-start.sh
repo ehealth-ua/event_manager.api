dff4313bf9f04d31af531fb39fa4da452bc42add
Alex Kovalevych <alexkovalevych@gmail.com>
2017-11-29 17:30:48 +0200

    [patch] add migrations release command

--- bin/hooks/pre-start.sh
+++ bin/hooks/pre-start.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-# `pwd` should be /opt/event_manager
 APP_NAME="event_manager"
 
 if [ "${DB_MIGRATE}" == "true" ]; then
   echo "[WARNING] Migrating database!"
-  ./bin/$APP_NAME command "${APP_NAME}_tasks" migrate!
+  ./bin/$APP_NAME command Elixir.EventManagerApi.ReleaseTasks seed
 fi;
